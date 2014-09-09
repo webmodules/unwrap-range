7cc94c67144066232c07630fb8511efe7ef83997
Nathan Rajlich <nathan@tootallnate.net>
2014-09-09 12:34:08 -0700

    index: remove dreadful console.log() calls

--- index.js
+++ index.js
@@ -35,9 +35,6 @@ function unwrap (range, nodeName, root, doc) {
   var startRight = findWithin(range.endContainer, Node.TEXT_NODE, false);
   var startRightOffset = range.startContainer.nodeType === Node.TEXT_NODE ? range.endOffset : startRight.nodeValue.length;
 
-  console.log('startLeft', range.startContainer, range.startOffset, startLeft, startLeftOffset);
-  console.log('startRight', range.endContainer, range.endOffset, startRight, startRightOffset);
-
   // check common ancestor container for `nodeName`
   var node = closest(range.commonAncestorContainer, nodeName, true, root);
   if (node) {
@@ -49,28 +46,21 @@ function unwrap (range, nodeName, root, doc) {
     var left = findWithin(outer.startContainer, Node.TEXT_NODE, true);
     var leftOffset = outer.startContainer.nodeType === Node.TEXT_NODE ? outer.startOffset : 0;
 
-    console.error('left:', left, leftOffset);
-
     if (left !== startLeft || leftOffset !== startLeftOffset) {
       debug('need to re-wrap left-hand side');
       r.setStart(left, leftOffset);
       r.setEnd(startLeft, startLeftOffset);
       var l = wrapRange(r, nodeName);
-      console.error('l:', l);
     }
 
     var right = findWithin(outer.endContainer, Node.TEXT_NODE, false);
     var rightOffset = outer.endContainer.nodeType === Node.TEXT_NODE ? outer.endOffset : right.nodeValue.length;
 
-    console.error('right:', right, rightOffset);
-
     if (right !== startRight || rightOffset !== startRightOffset) {
       debug('need to re-wrap right-hand side');
-      console.log('start right value', right.nodeValue, findWithin(outer.endContainer, Node.TEXT_NODE, false));
       r.setStart(startRight, startRightOffset);
       r.setEnd(right, rightOffset);
       var ri = wrapRange(r, nodeName);
-      console.error('ri:', ri);
     }
   }
 
